ee82baa902b65b9dd28b5cc826217f70b04035b9
Alex Nguyen Dinh <43184604+alexdinh1997@users.noreply.github.com>
2021-05-12 20:31:02 +0300

    Add files via upload

---
+++ Project-DataPipelines/airflow/plugins/operators/load_dimension.py
@@ -0,0 +1,34 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+
+    @apply_defaults
+    def __init__(self,
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # conn_id = your-connection-name
+                 conn_id,
+                 table,
+                 query,
+                 truncate=False,
+                 *args, **kwargs):
+       
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        # Map params here
+        # Example:
+        self.conn_id = conn_id
+        self.table=table
+        self.query=query
+        self.truncate=truncate
+
+        #truncate variable is for delete all row of table if existing
+
+    def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.conn_id)
+        if self.truncate:
+            redshift.run(f"TRUNCATE TABLE {self.table}")
+        redshift.run(f"INSERT TABLE {self.table} {self.query}")
---
+++ Project-DataPipelines/airflow/plugins/operators/stage_redshift.py
@@ -0,0 +1,46 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
+
+class StageToRedshiftOperator(BaseOperator):
+    ui_color = '#358140'
+
+    @apply_defaults
+    def __init__(self,
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 json_path="",
+                 *args, **kwargs):
+
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        # Map params here
+        # Example:
+        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.json_path=json_path
+
+
+    def execute(self, context):
+        self.log.info('StageToRedshiftOperator not implemented yet')
+
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+
+        self.log.info("Copying data from S3 to Redshift")
+        rendered_key = self.s3_key.format(**context)
+        s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
+        redshift.run(f"COPY {self.table} FROM '{s3_path}' ACCESS_KEY_ID '{credentials.access_key}' \
+            SECRET_ACCESS_KEY '{credentials.secret_key}' FORMAT AS JSON '{self.json_path}'")
